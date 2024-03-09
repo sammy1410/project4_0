8d7a044db63b9c7b0ac8c837e9d5e19803fbcf00
Sammy1410 <sammy.timsina14@gmail.com>
2024-03-09 11:34:22 +0545

    Try Run

--- try.py
+++ try.py
@@ -1,1 +1,20 @@
+import time 
+import datetime
 
+def timestamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def timecode():
+    current_time_seconds = int(time.time())
+    print(current_time_seconds)
+    code = current_time_seconds % 100000000
+    print(code)
+    return code
+
+def timestamp_int():
+    date_time_str = "2024-03-08 12:30:45"
+    date_time_obj = datetime.datetime.now().strptime(date_time_str, "%Y-%m-%d %H:%M:%S")
+    print(date_time_obj)
+    return date_time_obj
+
+timestamp_int().year
